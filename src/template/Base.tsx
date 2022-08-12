e28a8731c2f6a729fdedd641230787567b7182a2
unknown <adriansefcic@yahoo.ro>
2022-08-12 13:31:39 +0300

    facebook msg check

--- src/template/Base.tsx
+++ src/template/Base.tsx
@@ -1,5 +1,6 @@
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
+import { FacebookMsg } from './FacebookMsg';
 import { Footer } from './Footer';
 import { Home } from './Home';
 import { Mission } from './Mission';
@@ -16,6 +17,7 @@ const Base = (props: IBaseType) => (
   <div className="antialiased text-gray-700">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Navbar prefix="" />
+    <FacebookMsg />
     <Home />
     <Mission />
     <TeamCards />
---
+++ src/template/FacebookMsg.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @next/next/inline-script-id */
+import Script from 'next/script';
+
+function FacebookMsg() {
+  return (
+    <div>
+      <div id="fb-root"></div>
+
+      <div id="fb-customer-chat" className="fb-customerchat"></div>
+      <Script strategy="lazyOnload">
+        {`
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "849818798379340");
+            chatbox.setAttribute("attribution", "biz_inbox");
+      
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v14.0'
+              });
+            };
+      
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/ro_RO/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        `}
+      </Script>
+    </div>
+  );
+}
+
+export { FacebookMsg };
